97c15931c4daa32f20c03e00b984c6813e951548
BreElle Wells <breellejordyn@gmail.com>
2023-07-13 17:44:29 -0600

    set up an event on DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    const newText = document.getElementById('text')
+    newText.textContent = "This is really cool!"
+})
